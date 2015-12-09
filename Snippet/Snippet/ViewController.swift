fa95d5870f3bf01759600591d30217b0753cb426
rizakii <riza.alaudin@kii.com>
2015-12-09 16:01:03 +0900

    initial commit.

---
+++ Snippet/Snippet/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Snippet
+//
+//  Created by syahRiza on 12/9/15.
+//  Copyright © 2015 Kii. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
