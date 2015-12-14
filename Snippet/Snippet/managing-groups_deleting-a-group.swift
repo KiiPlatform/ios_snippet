56aa866372f47a96c3d04b9857a0c4108849867b
rizakii <riza.alaudin@kii.com>
2015-12-14 17:59:53 +0900

    Snippet for path /guides/ios/managing-groups/deleting-a-group/

---
+++ Snippet/Snippet/managing-groups_deleting-a-group.swift
@@ -0,0 +1,38 @@
+//
+//  managing-groups_deleting-a-group.swift
+//  Snippet
+//
+//  Created by syahRiza on 12/14/15.
+//  Copyright © 2015 Kii. All rights reserved.
+//
+
+import Foundation
+// MARK: path /guides/ios/managing-groups/deleting-a-group/
+private let groupUri = "groupURI"
+
+private func snippet_blocking(){
+    // Instantiate the group.
+    // (Assume that groupUri has the reference URI of the target group).
+    let group = KiiGroup(URI: groupUri)
+    var error : NSError?
+    
+    group.deleteSynchronous(&error)
+    
+    if error != nil {
+        //Error Handling
+        return
+    }
+}
+
+private func snippet_non_blocking(){
+    // Get the currently logged in user.
+    let group = KiiGroup(URI: groupUri)
+    
+    group.deleteWithBlock { (user, error) -> Void in
+        if error != nil {
+            //Error Handling
+            return
+        }
+    }
+    
+}
