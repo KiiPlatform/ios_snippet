1dfa0973dd5a4f9cd9f128c3edd23c20528501dd
rizakii <riza.alaudin@kii.com>
2015-12-14 19:25:59 +0900

    Snippet for en/guides/ios/managing-data/object-storages/deleting/

---
+++ Snippet/Snippet/ios_managing-data_buckets_deleting.swift
@@ -0,0 +1,33 @@
+//
+//  ios_managing-data_buckets_deleting.swift
+//  Snippet
+//
+//  Created by syahRiza on 12/14/15.
+//  Copyright © 2015 Kii. All rights reserved.
+//
+
+import Foundation
+// MARK: path : en/guides/ios/managing-data/object-storages/deleting/
+private func snippet_blocking(){
+    var error : NSError?
+    
+    let bucket = Kii.bucketWithName("__KII_BUCKET_")
+    
+    bucket.deleteSynchronous(&error)
+    if (error != nil) {
+        // Error handling
+        return
+    }
+    
+}
+
+private func snippet_non_blocking(){
+    let bucket = Kii.bucketWithName("__KII_BUCKET_")
+    
+    bucket.deleteWithBlock { (retObj, error) -> Void in
+        if (error != nil) {
+            // Error handling
+            return
+        }
+    }
+}
