325515962a8c993e0c8e2ddb9b5036d46d2fdcb0
rizakii <riza.alaudin@kii.com>
2015-12-15 17:52:55 +0900

    Snippet for path : en/guides/ios/managing-data/object-storages/uploading/resume

---
+++ Snippet/Snippet/ios_managing-data_object-storages_uploading_suspend.swift
@@ -0,0 +1,22 @@
+//
+//  ios_managing-data_object-storages_uploading_suspend.swift
+//  Snippet
+//
+//  Created by syahRiza on 12/15/15.
+//  Copyright © 2015 Kii. All rights reserved.
+//
+
+import Foundation
+// MARK: path : en/guides/ios/managing-data/object-storages/uploading/resume
+private let mUploader = KiiObject(URI: "dummy").uploader("dummy")
+
+private func snippet(){
+    var error : NSError?
+    
+    mUploader.suspend(&error)
+    
+    if error != nil {
+        // Error handling
+        return
+    }
+}
