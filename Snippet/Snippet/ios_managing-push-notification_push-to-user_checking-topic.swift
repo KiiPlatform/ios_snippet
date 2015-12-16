07aad5854e325ef5d42241703d6077b055fb664f
rizakii <riza.alaudin@kii.com>
2015-12-16 14:09:31 +0900

    Snippet for path managing-push-notification/push-to-user/checking-topic/

---
+++ Snippet/Snippet/ios_managing-push-notification_push-to-user_checking-topic.swift
@@ -0,0 +1,31 @@
+//
+//  ios_managing-push-notification_push-to-user_checking-topic.swift
+//  Snippet
+//
+//  Created by syahRiza on 12/16/15.
+//  Copyright © 2015 Kii. All rights reserved.
+//
+
+import Foundation
+//MARK: path managing-push-notification/push-to-user/checking-topic/
+
+private let topic = KiiUser.currentUser().topicWithName("dummy")
+
+private func snippet_blocking(){
+    // currently is not supported (swift compiler problem)
+    
+}
+private func snippet_non_blocking(){
+    // Assume that "topic" is already instantiated.
+    topic.checkIfExists { (topic, isExists, error ) -> Void in
+        if error != nil {
+            //Error handling
+            return
+        }
+        if (isExists) {
+            print("Topic is exists!")
+        } else {
+            print("Topic is not exists!")
+        }
+    }
+}
