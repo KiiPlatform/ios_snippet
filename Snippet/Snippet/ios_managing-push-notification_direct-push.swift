3a8c39d24ce22edc0b6b91360856a8c6ecdacfc5
rizakii <riza.alaudin@kii.com>
2015-12-16 18:18:01 +0900

    Snippet for path managing-push-notification/direct-push/

---
+++ Snippet/Snippet/ios_managing-push-notification_direct-push.swift
@@ -0,0 +1,24 @@
+//
+//  ios_managing-push-notification_direct-push.swift
+//  Snippet
+//
+//  Created by syahRiza on 12/16/15.
+//  Copyright © 2015 Kii. All rights reserved.
+//
+
+import Foundation
+//MARK: path managing-push-notification/direct-push/
+private func snippet(){
+    //snippet starts here
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        print("Received notification : \(userInfo)")
+        
+        // Create KiiPushMessage from userInfo.
+        let message = KiiPushMessage(fromAPNS: userInfo)
+        // Get Topic string using getValueOfKiiMessageField. "KiiMessage_TOPIC" is enum that is defined in KiiMessageField.
+        let title = message.getValueOfKiiMessageField(.TOPIC)
+        // Show alert message
+        message.showMessageAlertWithTitle(title)
+        
+    }
+}
