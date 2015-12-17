81f6e7bd5cffd96f7bcabbbb1126db8b3c6a2930
rizakii <riza.alaudin@kii.com>
2015-12-17 17:21:33 +0900

    Snippet for path guides/thingifsdk/thingsdk/thing-client/deleting-things

---
+++ Snippet/Snippet/guides_thingifsdk_thingsdk_thing-client_deleting-things.swift
@@ -0,0 +1,41 @@
+//
+//  guides_thingifsdk_thingsdk_thing-client_deleting-things.swift
+//  Snippet
+//
+//  Created by syahRiza on 12/17/15.
+//  Copyright © 2015 Kii. All rights reserved.
+//
+
+import Foundation
+// MARK: path guides/thingifsdk/thingsdk/thing-client/deleting-things
+
+private func snippet_blocking(){
+    let thing : KiiThing
+    do{
+        thing = try KiiThing.loadSynchronousWithVendorThingID("rBnvSPOXBDF9r29GJeGS")
+    }catch(let error as NSError){
+        //Error Handling
+        print(error)
+        return;
+    }
+    var error : NSError?
+    thing.deleteSynchronous(&error)
+    if error != nil {
+        // Error handling
+        return;
+    }
+}
+private func snippet_non_blocking(){
+    KiiThing.loadWithVendorThingID("rBnvSPOXBDF9r29GJeGS") { (thing, error ) -> Void in
+        if error != nil {
+            // Error handling
+            return;
+        }
+        thing.delete({ (deletedThing, error ) -> Void in
+            if error != nil {
+                // Error handling
+                return;
+            }
+        })
+    }
+}
